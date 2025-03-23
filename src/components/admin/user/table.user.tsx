b053d5482cd74a40093b0b5289b287d076b293fc
locnht <locnht.it@gmail.com>
2025-03-23 17:49:07 +0700

    video 57

--- src/components/admin/user/table.user.tsx
+++ src/components/admin/user/table.user.tsx
@@ -16,6 +16,7 @@ import DetailUser from "./detail.user";
 import CreateUser from "./create.user";
 import ImportUser from "./data/import.user";
 import { CSVLink } from "react-csv";
+import UpdateUser from "./update.user";
 
 type TSearch = {
   fullName: string;
@@ -38,6 +39,9 @@ const TableUser = () => {
   const [openModalCreate, setOpenModalCreate] = useState<boolean>(false);
   const [openModalImport, setOpenModalImport] = useState<boolean>(false);
   const [currentDataTable, setCurrentDataTable] = useState<IUserTable[]>([]);
+
+  const [openModalUpdate, setOpenModalUpdate] = useState<boolean>(false);
+  const [dataUpdate, setDataUpdate] = useState<IUserTable | null>(null);
 
   const columns: ProColumns<IUserTable>[] = [
     {
@@ -94,6 +98,10 @@ const TableUser = () => {
             <EditTwoTone
               twoToneColor="#f57800"
               style={{ cursor: "pointer", marginRight: 15 }}
+              onClick={() => {
+                setDataUpdate(entity);
+                setOpenModalUpdate(true);
+              }}
             />
             <DeleteTwoTone
               twoToneColor="#ff4d4f"
@@ -212,6 +220,14 @@ const TableUser = () => {
       <ImportUser
         openModalImport={openModalImport}
         setOpenModalImport={setOpenModalImport}
+        refreshTable={refreshTable}
+      />
+
+      <UpdateUser
+        openModalUpdate={openModalUpdate}
+        setOpenModalUpdate={setOpenModalUpdate}
+        dataUpdate={dataUpdate}
+        setDataUpdate={setDataUpdate}
         refreshTable={refreshTable}
       />
     </>
